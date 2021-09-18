078f295bb2f0bde413e4178de2098b710f6878f4
unknown <jhonnatha16011987@yahoo.com.br>
2021-09-17 22:12:25 -0300

    Adicionado JpaRepository

--- src/test/java/com/jhonnatha/pontointeligente/api/PontoInteligenteApplicationTests.java
+++ src/test/java/com/jhonnatha/pontointeligente/api/PontoInteligenteApplicationTests.java
@@ -1,9 +1,11 @@
 package com.jhonnatha.pontointeligente.api;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class PontoInteligenteApplicationTests {
 
 	@Test
